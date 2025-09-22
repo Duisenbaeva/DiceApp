8c5ad6aa16f84941b3e0eabc689274744e8c61e2
Duisenbaeva <diana.duisenbaeva@icloud.com>
2025-09-22 16:10:02 +0500

    Add full Xcode project

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Button: UIButton!
+    @IBOutlet weak var dice1: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        dice1.image = UIImage(named: "dice-3")
+        Button.backgroundColor = UIColor.blue
     }
 
 
+    @IBAction func throwButton(_ sender: Any) {
+        print("User tapped")
+    }
+    
+    
 }
 
